c06b5d3fe744792e5b85b6fe911075928e5d4cba
Vasanth <vasanth.g@adomita.com>
2015-01-20 17:30:30 +0530

    The final code
    Described both the functions through proper definition

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,17 @@
-## A R Program for cacheing time consuming computations, by preserving the state inside a R Object
+## The following code is a  R Program for cacheing time consuming computations, by preserving the state inside a R Object
 ## There are two functions in program..1) makeCacheMatrix()  and ..2) caschesolve()
 
-## Definition of makeCacheMatrix() function..This function creates a special matrix object that can cache its inverse
-## This function takes a matrix function as its argument 
+## Definition of makeCacheMatrix() function..
+##-----------------------------------------------
+
+##This function creates a special matrix object that can cache its inverse
+## This function takes a matrix () function as its argument 
 ## m is initialized to null value
 ## we define a set() function which uses the `<<` operator to assign the values in the 
 ##defining environment ( making use of lexical scoping). The value of Y is assigned to x and NULL to m within the function
-
+## if suppose a<- makeCacheMatrix(matrix(c(1,0,5,  2,1,6,  3,5,0),nrow=3, ncol=3)) is computed
+## a$get() gives us the input matrix
+## cachesolve(a)...This is the second function, gives us the inverse of the matrix..which is the cached data.
 makeCacheMatrix <- function(x = matrix()) {
    m <- NULL
    set <- function(y) {
@@ -21,8 +26,12 @@ makeCacheMatrix <- function(x = matrix()) {
         getmatinverse = getmatinverse)
  }
 
-## Write a short comment describing this function
 
+## Definition of cachesolve() function
+##--------------------------------------
+## This is the second function which returns the cached data. That is the inverse of the matrix
+## The inverse of the matrix is calculated by the usage of the solve() function
+## after computing the cachesolve(), the a$getmatinverse() returns the inverse of the matrix. That is the cached data
 cachesolve <- function(x, ...) {
    m <- x$getmatinverse()
    if(!is.null(m)) {
@@ -35,3 +44,34 @@ cachesolve <- function(x, ...) {
    m
  }
  
+
+
+####...example of runing the above code(optional)
+##----------------------------------------------------
+
+a<- makeCacheMatrix(matrix(c(1,0,5,  2,1,6,  3,5,0),nrow=3, ncol=3))## Taking a 3 by 3 matrix as an argument for the makeCacheMatrix() function
+
+> a$get() ## Checking the input matrix
+
+      [,1] [,2] [,3]
+[1,]    1    2    3
+[2,]    0    1    5
+[3,]    5    6    0
+
+
+> cachesolve(a) ## The below is the desired output ie, it is the inverse of the input matrix
+
+
+       [,1] [,2] [,3]
+[1,]   -6  3.6  1.4
+[2,]    5 -3.0 -1.0
+[3,]   -1  0.8  0.2
+
+
+> a$getmatinverse()
+
+      [,1] [,2] [,3]
+[1,]   -6  3.6  1.4
+[2,]    5 -3.0 -1.0
+[3,]   -1  0.8  0.2
+>
